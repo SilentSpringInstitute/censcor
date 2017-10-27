d5fd9cb8d31b0550209423e349c22821014b80be
Herb Susmann <herbps10@gmail.com>
2017-10-27 17:27:04 -0400

    Basic package documentation

---
+++ R/censR-package.R
@@ -0,0 +1,16 @@
+#' Bayesian Estimation of Correlations between Censored Variables
+#' 
+#' A set of tools for fully Bayesian estimation of correlations between left and interval censored variables.
+#' 
+#' @docType package
+#' @name censR-package
+#' @aliases censR
+#' 
+#' @importFrom rstan sampling
+#' 
+#' @importFrom MASS mvrnorm
+#' 
+#' @import stats
+#' 
+#' @useDynLib censR, .registration = TRUE
+NULL
