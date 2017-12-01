d5d1e90f5391c09ccc211976ce39b56637e17c93
Herb Susmann <herbps10@gmail.com>
2017-12-01 14:47:53 -0500

    Revert "Updated tolerance on test"
    This reverts commit e90b0bcef6fa785283cb5753545c1b3668c1b89a.

--- tests/testthat/test_spearman_kendall.R
+++ tests/testthat/test_spearman_kendall.R
@@ -8,10 +8,12 @@ test_that("kendall's tau and spearman correlation estimation returns accurate va
     expect_equal(rstan::summary(fit)$summary["rho", "50%"], 0.435, tol = 0.001)
     expect_equal(rstan::summary(fit)$summary["tau", "50%"], 0.287, tol = 0.001)
     
+    #NOTE -- this may also produce correct spearman correlation estimates?
+    
     set.seed(10)
     d1 <- generate_censored_data(100, -0.75, 0, direction = -1)
     fit <- censcor(x ~ y, d1, seed = 1, iter = 600, method = "kendall")
     expect_equal(rstan::summary(fit)$summary["rho", "50%"], -0.73, tol = 0.001)
-    expect_equal(rstan::summary(fit)$summary["tau", "50%"], -0.52, tol = 0.01)
+    expect_equal(rstan::summary(fit)$summary["tau", "50%"], -0.52, tol = 0.001)
   })
 })
